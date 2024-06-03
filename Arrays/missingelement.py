819fdd85c432d33e377a22556d0f00e9b4b1a04e
narayanan.cp.official@gmail.com <narayanan.cp.official@gmail.com>
2024-06-03 19:58:53 +0530

    Done

---
+++ Arrays/missingelement.py
@@ -0,0 +1,19 @@
+def missingNumber(array,n):
+        # create a list of zeroes
+    temp = [0] * (n)
+
+    for i in range(0, n-1):
+        temp[array[i] - 1] = 1
+
+    for i in range(0, n+1):
+        if(temp[i] == 0):
+            ans = i + 1
+            break
+    return ans  
+
+array = [1, 2, 3, 5]
+n = 5
+
+   
+print(missingNumber(array, n))   
+    
